f7ac331cc910ce2165f008f2d326650188619c44
방지훈 <bangjihun@bangjihun-ui-MacBookPro.local>
2023-07-31 23:29:58 +0900

    classlist_toggle

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
     const clickedClass = "clicked";
+    // toggle 은 classlist에 classname이 존재하면 삭제, 존재하지 않는다면 classname을 추가해준다.
+    // 아래의 if문의 동작을 한줄로 표현.
+    h1.classList.toggle("clicked");
 
+    /*
     if(h1.classList.contains(clickedClass)) {
         h1.classList.remove(clickedClass);
     } else {
         h1.classList.add(clickedClass);
     }
+*/
 }
 
 h1.addEventListener("click", handleTitleClick);
