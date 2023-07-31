d971c589e60b9e06aebd2792ed20be913212adbf
방지훈 <bangjihun@bangjihun-ui-MacBookPro.local>
2023-07-31 23:08:34 +0900

    JS_IF문

--- app.js
+++ app.js
@@ -1,63 +1,13 @@
-// event listen하기
+const h1 = document.querySelector("div.hello:first-child h1")
 
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    h1.style.color = "blue";
+function handleMouseEnter(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue"){
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-    alert("copier!");
-}
-function handleWindowPaste(){
-    alert("pasted!");
-}
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-
-// h1.addEventLister("click", handleTitleClick);
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.onmouseleave = handleMouseLeave;
-
-//window, document 는 기본적으로 제공해주는 함수
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("paste", handleWindowPaste);
-window.addEventListener("offline", handleWindowOffline); // wifi 껐을때.
-window.addEventListener("offline", handleWindowOnline); // wifi 켰을떄.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+h1.addEventListener("click", handleMouseEnter);
